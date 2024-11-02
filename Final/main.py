940e688a8ac7d123958ebf6bad312fa22a3019d9
Jacob Brockett <brockett04@gmail.com>
2024-11-02 12:34:37 -0500

    added TODOs

--- Final/main.py
+++ Final/main.py
@@ -6,9 +6,23 @@ lib.initSerComm()
 
 parked = False
 
+# TODO: check touch sensor to start the program
+
 while not parked:
 
+    # move forward
 
+    # TODO: check space color thread
+
+    # stop and begin turn sequence
+
+    # TODO: check gyro thread
+
+    # stop and begin forward sequence
+
+    # TODO: check parking distance thread
+
+    # stop and end program
 
     break
 
