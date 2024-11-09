d430d6193d8407faebfbdffdc153f47eedd4fc5f
Jacob Brockett <102402828+jacobbrockett@users.noreply.github.com>
2024-11-09 11:30:35 -0600

    added touch to start (#9)

--- Final/main.py
+++ Final/main.py
@@ -1,11 +1,14 @@
 import threading
 import mySerCommLibrary
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH) # Configure for a touch sensor. If an EV3 touch sensor is connected, it will be configured for EV3 touch, otherwise it'll configured for NXT touch.
 
 lib = mySerCommLibrary.SerialComm(9600, "/dev/ttyUSB0", 1)
 
 lib.initSerComm()
 
-parked = False
 
 #thread for color detectioncolor_thread = threading.Thread(target=check_color)
 
@@ -13,7 +16,17 @@ threading.Thread(target=lib.check_color).start()
 
 
 
-# TODO: check touch sensor to start the program
+# Check touch sensor to start the program
+print("Press touch sensor on port 1 to check color")
+touch = 0
+while not touch:
+    try:
+        touch = BP.get_sensor(BP.PORT_1)
+    except brickpi3.SensorError:
+        pass
+
+# Main Parking Program:
+parked = False
 
 while not parked:
 
