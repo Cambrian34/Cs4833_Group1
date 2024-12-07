0c5e74a507053e487a8f930b3ef27e9813573931
Alistair Chambers <38084528+Cambrian34@users.noreply.github.com>
2024-12-07 13:00:52 -0600

    Final changes

--- Final/main.py
+++ Final/main.py
@@ -1,8 +1,27 @@
 import threading
 import time
+import sys
 
 import mySerCommLibrary
 import brickpi3 # import the BrickPi3 drivers
+
+# Get Command Line Arguments:
+args = sys.argv
+if len(args) < 2:
+    print("Error: Must specify Student, Faculty, or Reserved")
+    print("Example usage: python main.py Student")
+    exit(1)
+
+target_color = ""
+if args[1] == "Student":
+    target_color = "Blue"
+elif args[1] == "Faculty":
+    target_color = "Green"
+elif args[1] == "Reserved":
+    target_color = "Red"
+else:
+    print("Error: Invalid input, must specify Student, Faculty, or Reserved")
+    exit(1)
 
 # Setup Comms and Sensors:
 serial_comm = mySerCommLibrary.SerialComm(9600, "/dev/ttyUSB0", 1)
@@ -27,45 +46,50 @@ try:
     while not parked:
 
         # Move Forward:
-        serial_comm.moveForward(15)
+        serial_comm.moveForward(17)
 
         # Check space color thread
         print("Color Thread Start")
         while True:
             print(brickpi_sensors.color_detected)
-            if brickpi_sensors.color_detected == "Blue":
+            if brickpi_sensors.color_detected == target_color:
+                print("Target color ", target_color, " detected")
                 serial_comm.stop_robot()
                 break
+            elif brickpi_sensors.color_detected == "Yellow":
+                serial_comm.stop_robot()
+                print("Target color ",brickpi_sensors.color_detected , " detected")
+                print("No available spaces!")
+                raise KeyboardInterrupt  # stop program from continuing
 
         print("begin turn")
 
         # begin turn sequence
         serial_comm.turnLeft(20)
-        time.sleep(1.8) # TODO: calibrate turning
+        time.sleep(1.8)
         serial_comm.stop_robot()
 
-        # TODO: check gyro thread
 
         # check if space available. if true, park; if not, turn back and continue searching for a space
 
-        # TODO: check parking distance thread, an error where the thread and library call the arduino at same time coausing an erro that the arduino is busy,
-        #ie a critical section problem
-        #could solving this by using a semaphore or mutex
+        # Check Parking Distance:
         # print(type(serial_comm.readSonicCM(3)))
         initial_distance = serial_comm.readSonicCM(3)
         print("Parking Distance ", initial_distance)
         # print ("Type ", type(initial_distance))
         if  initial_distance >= 30:
             print("Space Available")
-            serial_comm.moveForward(15)
-            while serial_comm.readSonicCM(3) > 15:
-                time.sleep(0.1)
+            serial_comm.moveForward(12)
+            while serial_comm.readSonicCM(3) > 22:
+                time.sleep(0.05)
 
             serial_comm.stop_robot()
             parked = True
         else:
+            serial_comm.flashRed()
+            time.sleep(2)
             serial_comm.moveBack(15)
-            time.sleep(1.6)
+            time.sleep(1.1)
             serial_comm.stop_robot()
             time.sleep(0.5)
             serial_comm.turnRight(20)
@@ -74,9 +98,12 @@ try:
 
 
         # stop and end program
+    serial_comm.flashGreen()
+    raise KeyboardInterrupt  # stop program from continuing
 
 except KeyboardInterrupt:
     print("Exit")
     color_thread.join()
-    exit(1)
+    brickpi_sensors.reset_sensors()
+    sys.exit()
 
